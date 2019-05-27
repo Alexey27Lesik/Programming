aa8df3a7f9311b356bdaeaf8a492d61a29849fd9
Alexey27Lesik <42997318+Alexey27Lesik@users.noreply.github.com>
2019-05-27 23:48:04 +0300

    Add files via upload

---
+++ test_LEquation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        cout<<"Test_1\nEnter k\n1 2 3 4 -1"<<endl;
+        cout<<"If result: \nLinear equation: x_1 = -2\nQuadratic equation: x_1 = -1 + i*1.41421; x_2 = -1 - i*1.41421\nCubic equation: x_1 = -1.65063; x_2 = -0.174685 + i*-1.54687; x_3 = -0.174685 - i*-1.54687\nQuatre equation: x_1 = -1.76565; x_2 = 0.211299; x_3 = -0.222824 + i*1.62195; x_4 = -0.222824 - i*1.62195\n"<<endl<<"Tested_1";
+        cout<<"Test_1\nEnter k\n3 4 9 3 9"<<endl;
+        cout<<"If result:\nLinear equation: x_1 = -1.33333\nQuadratic equation: x_1 = -0.666667 + i*1.59861; x_2 = -0.666667 - i*1.59861\nCubic equation: x_1 = -0.379034; x_2 = -0.47715 + i*1.55262; x_3 = -0.47715 - i*1.55262\nQuatre equation: x_1 = 0.207554 + i*-1.06488; x_2 = 0.207554 - i*-1.06488; x_3 = -0.874221 + i*1.33584; x_4 = -0.874221 - i*1.33584\n"<<endl<<"Tested_2";
+    return 0;
+}
